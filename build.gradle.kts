ad4694171e363f4c1d10daaf7c406f0856a751de
maanrit <108694058+maanrit@users.noreply.github.com>
2022-11-21 21:54:39 +0100

    set up for dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,17 @@ plugins {
     id("java")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "patiently"
+version = "0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("org.assertj:assertj-core:3.23.1")
+    implementation("org.mockito:mockito-core:4.8.1")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
 tasks.getByName<Test>("test") {
---
+++ src/test/java/patiently/TestSomething.java
@@ -0,0 +1,12 @@
+package patiently;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestSomething {
+    @Test
+    public void testSomething() {
+        assertThat(1).isEqualTo(1);
+    }
+}
